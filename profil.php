fd24102b032688aedb4725dde01b9cebcb9ed581
Ahp06 <axno@hotmail.fr>
2017-06-07 11:16:04 +0200

    Add files via upload
    Modif profil importation photo

--- profil.php
+++ profil.php
@@ -212,64 +212,73 @@ setTimeout('refresh_liste()', 100);
 		
 			
 		<div id ="Publication" >
-			<form name="Publier" method="post">
+			<form name="Publier" method="post" enctype="multipart/form-data">
 		
 				<input type="textarea" placeholder="Un titre" name="titre" style="height: 5%; width: 100%">
 				<input type="textarea" placeholder="Où étiez-vous ? " name="position" style="height: 5%; width: 100%">
 				<input type="textarea" placeholder="Rédigez votre publication ici" name="contenu" style="height: 10%; width: 100%"> 
 				<input type ="submit" name="Publier" value="Publier" >
 				<input type ="submit" name="Photo/video" value="Photo/vidéo" >
-				
+				<input type="hidden" name="MAX_FILE_SIZE" value="100000"> Fichier : <input type="file" name="fichier"> <!-- onchange="this.form.submit()" -->
 				
 			</form>
+			
 		</div>
 		
 		
 
 		<div id="list">
-		// <?php
+		<?php
 
-			// echo('<h1> Amis connectés :</h1>');
-			// $req = $bdd->query('SELECT nom,prenom from utilisateur where uha =\''.$login.'\' ');  
-			// $rep = $bdd->query('SELECT nom,prenom FROM utilisateur WHERE connecte=1 AND uha !=\''.$login.'\' '); 
+			echo('<h1> Amis connectés :</h1>');
+			$req = $bdd->query('SELECT nom,prenom from utilisateur where uha =\''.$login.'\' ');  
+			$rep = $bdd->query('SELECT nom,prenom FROM utilisateur WHERE connecte=1 AND uha !=\''.$login.'\' '); 
 			
-			// echo('<ul>');
+			echo('<ul>');
 		
-			// while($donnees=$rep->fetch()){
-				// echo('<li>'.$donnees['nom'].' '.$donnees['prenom'].'</li>');
-				// }
-			// echo('</ul>');
+			while($donnees=$rep->fetch()){
+				echo('<li>'.$donnees['nom'].' '.$donnees['prenom'].'</li>');
+				}
+			echo('</ul>');
 
-			// echo('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>');
+			echo('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>');
 
-			// echo('<h1> Ami hors ligne :</h1>');
-			// $rep = $bdd->query('SELECT nom,prenom FROM utilisateur WHERE connecte=0');
-			// echo('<ul>');
+			echo('<h1> Ami hors ligne :</h1>');
+			$rep = $bdd->query('SELECT nom,prenom FROM utilisateur WHERE connecte=0');
+			echo('<ul>');
 		
-			// while($donnees=$rep->fetch()){
-				// echo('<li>'.$donnees['nom'].' '.$donnees['prenom'].'</li>');
-			// }
-			// echo('</ul>');
-		// ?>
+			while($donnees=$rep->fetch()){
+				echo('<li>'.$donnees['nom'].' '.$donnees['prenom'].'</li>');
+			}
+			echo('</ul>');
+		?>
 		</div>
 	
 		<div id="actualite">
 		<?php
+			
+
+			
 			if(isset($_POST['Publier'])){ 
 				if(!empty($_POST['contenu'])){ 
-				echo '<p> OK ! </p>' ; 
 				
-			 
+				
 				$contenu = $_POST['contenu'];
 				$titre = $_POST['titre']; 
 				$time = date("Y-m-d H:i:s");
+				$fichier = $_FILES['fichier']['name'] ; 
 				
 				include('./traitement/mots_interdits.php'); 
-			if($existe == FALSE ){ 
-			$insert_actualite = $bdd->prepare('INSERT INTO actualite(titre,contenu,position,fichier,date) VALUES( :titre , :contenu,:position, \'\' ,\''.$time.'\')'); 
-			$insert_actualite->execute(array('titre' => $_POST['titre'], 'contenu' => $_POST['contenu'], 'position' => $_POST['position']));
-				$id_utilisateur = $bdd->query('SELECT id from utilisateur where uha =\''.$login.'\' '); 
-						$id_actualite = $bdd ->prepare('SELECT id from actualite where titre = :titre and contenu = :contenu') ; 
+				
+			if($existe == FALSE ){
+			
+			$insert_actualite = $bdd->prepare('INSERT INTO actualite(titre,contenu,position,fichier,date) VALUES( :titre , :contenu,:position, :fichier ,\''.$time.'\')'); 
+			$insert_actualite->execute(array('titre' => $_POST['titre'], 'contenu' => $_POST['contenu'], 'position' => $_POST['position'] , 'fichier' =>$_FILES['fichier']['name']));
+			
+			include('./traitement/upload.php'); 
+			
+			$id_utilisateur = $bdd->query('SELECT id from utilisateur where uha =\''.$login.'\' '); 
+			$id_actualite = $bdd ->prepare('SELECT id from actualite where titre = :titre and contenu = :contenu') ; 
 			$id_actualite->execute(array('titre' => $_POST['titre'], 'contenu' => $_POST['contenu']));
 			
 				$id_uti = $id_utilisateur->fetch();
@@ -279,6 +288,7 @@ setTimeout('refresh_liste()', 100);
 			
 				header("location:profil.php"); 
 			}
+			
 			}
 		
 			else{
@@ -293,22 +303,47 @@ setTimeout('refresh_liste()', 100);
 		echo('<h2> Mes publications </h2>');
 		$rep = $bdd->query('SELECT * FROM actualite INNER JOIN post on actualite.id = post.idact INNER JOIN utilisateur on post.iduti = utilisateur.id where utilisateur.id = \''.$id_utilisateur[0].'\' ORDER BY date desc');
 
+		if (isset($_FILES['fichier'])){
+			echo $_FILES['fichier']['name'] ; 
+		}
+		
 		include('./traitement/smiley.php'); 
 		while($donnees=$rep->fetch()){
 			echo('<div class="div_news">');
 			$id_actualite = $bdd ->query('SELECT id from actualite where contenu = \''.$donnees['contenu'].'\' and titre = \''.$donnees['titre'].'\' ') ; 
 			$id = $id_actualite->fetch(); 
-						?>	
+			echo $id[0]; 
+			
+			$f = $bdd->query('SELECT fichier FROM actualite INNER JOIN image ON image.idact = actualite.id AND actualite.id = \''.$id[0].'\' ' ); 
+			$name_file = $f ->fetch(); 
+			echo $name_file[0];
+			
+			
+			?>	
 				<a href='./traitement/deleteOnProfile.php?id=<?php echo $id[0]; ?> '>Supprimer</a>
 			<?php 
-
 			echo('<h2>'.$donnees['titre'].'</h2>');
+			
+			//Rajouter un if (image.idact = 
+			//echo "<img src=\'./uploaded/'.\"".$name_file[0]."\">" ; 
+			//remplacer par $donnees['fichier'] 
+			?>
+				<img src="./uploaded/'".$donnees['fichier']'." width="200" height="200">  
+			<?php
+			
 			echo('<p>'.filtre_texte($donnees['contenu']).'<p>');
 			if($donnees['position'] != ''){
 				echo('<p>'.'À '.$donnees['position'].'</p>'); 
 			}
 			echo('</br>');
 			echo('<p>'.$donnees['date'].'<p>');
+			?>
+			
+			<form name="Commenter" method="post" >
+				<input type ="submit" name="Commenter" value="Commenter" >
+			</form>
+			
+			<?php
 			echo('</div>');
 }
 
